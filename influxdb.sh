1e3274081809d7ee751fdb399344a42ab6f6691c
Dennis Eisold <github@eisold-edv.de>
2018-10-07 15:33:36 +0200

    Update influxdb.sh

--- influxdb.sh
+++ influxdb.sh
@@ -1,3 +1,3 @@
 cd /sys/class/net/enp4s0/statistics
 out=$(echo "STATUS" | nc 10.83.186.72 1234)
-/srv/graphana.py --rx $(cat rx_bytes) --tx $(cat tx_bytes) --mp $(echo $out | cut -d# -f2) --ps $(echo $out | cut -d# -f3) --con $(echo $out | cut -d# -f1)
+/srv/influxdb.py --rx $(cat rx_bytes) --tx $(cat tx_bytes) --mp $(echo $out | cut -d# -f2) --ps $(echo $out | cut -d# -f3) --con $(echo $out | cut -d# -f1)
